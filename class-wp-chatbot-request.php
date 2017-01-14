5ef4e022c6362cb0ed3364c38ac942c7852a1511
Kristoffer Lorentsen <krilor@gmail.com>
2017-01-14 08:54:11 +0100

    Adds WP_CHATBOT_USER param

--- class-wp-chatbot-request.php
+++ class-wp-chatbot-request.php
@@ -49,6 +49,9 @@ class WP_Chatbot_Request {
 					case 'WP_CHATBOT_CONV':
 						$value = $conv;
 						break;
+					case 'WP_CHATBOT_USER':
+						$value = $user;
+						break;
 					default:
 						$value = $this->options[$option_id . '-val'];
 				}
@@ -88,7 +91,7 @@ class WP_Chatbot_Request {
       // TODO: CHECK FOR SUCCESS and error scenarios
       $response_body = json_decode( wp_remote_retrieve_body ( $response ), true );
 			$bot_response = jsonPath($response_body, $this->options['response-jsonpath']);
-			
+
       //$bot_response = $response_body[$this->options['response-key-msg']];
 			//$bot_response = $response_body['result']['fulfillment']['messages'][0]['speech'];
     }
