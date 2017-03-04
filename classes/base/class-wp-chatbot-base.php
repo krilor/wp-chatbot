e14bf99c054968f0230b2cbcbc59d97cac75dcee
Kristoffer Lorentsen <krilor@gmail.com>
2017-03-04 23:46:17 +0100

    Removes loader class - fixes #20. Moves main plugin class to main file and introduce singleton pattern fixes #21.

---
+++ classes/base/class-wp-chatbot-base.php
@@ -0,0 +1,86 @@
+<?php
+
+
+if ( ! defined( 'WPINC' ) ) {
+	die;
+}
+
+if ( ! class_exists( 'WP_Chatbot_base' ) ):
+
+class WP_Chatbot_base {
+  /* Singleton */
+
+  /**
+   * The one and only plugin instance
+   */
+  protected static $instance;
+
+  /**
+   * The plugin directory without trailing slash
+   */
+  public $path;
+
+  /**
+   * The plugin url without trailing slash
+   */
+  public $url;
+
+	/**
+	 * The plugin name
+	 */
+	public $name;
+
+  /**
+   * The plugin slug
+   */
+  public $slug;
+
+  /**
+   * The plugin version
+   */
+  public $version;
+
+
+  /**
+   * Constructor of the class
+   *
+   * Does some lifing, then calls init().
+   * All plugin construct functionality should be in method init().
+   */
+  protected function __construct() {
+
+    $this->path = untrailingslashit( plugin_dir_path( $this->get_class_filename( ) ) );
+    $this->url  = untrailingslashit( plugin_dir_path( $this->get_class_filename( ) ) );
+		$this->name = str_replace( '_', ' ', get_class( $this ) );
+    $this->slug = strtolower( str_replace( '_', '-', get_class( $this ) ) );
+    $this->version = '1.0.0';
+
+    // l18n
+    add_action( 'plugins_loaded', array( $this, 'load_plugin_textdomain' ) );
+
+  }
+
+  /**
+   * Get the filename of the file of the last inherited class
+   * From: http://stackoverflow.com/questions/3014254/how-to-get-the-path-of-a-derived-class-from-an-inherited-method
+   */
+  protected function get_class_filename() {
+    $reflection_class = new ReflectionClass( get_class( $this ) );
+    return $reflection_class->getFileName( );
+  }
+
+  public function load_plugin_textdomain() {
+
+    load_plugin_textdomain(
+      $this->slug,
+      false,
+      $this->path . '/languages/'
+    );
+
+  }
+
+}
+
+
+
+endif; // WP_Chatbot_base exists
--- wp-chatbot.php
+++ wp-chatbot.php
@@ -24,23 +24,122 @@ if ( ! defined( 'WPINC' ) ) {
 }
 
 /**
- * The core plugin class that is used to define internationalization,
- * admin-specific hooks, and public-facing site hooks.
+* Base classes must be included first
+*/
+require_once plugin_dir_path( __FILE__ ) . 'classes/base/class-wp-chatbot-base.php';
+
+
+/**
+ * The core plugin class.
+ *
+ * This is used to define internationalization, admin-specific hooks, and
+ * public-facing site hooks.
+ *
+ * Also maintains the unique identifier of this plugin as well as the current
+ * version of the plugin.
+ *
+ * @since      0.1.0
+ * @package    Plugin_Name
+ * @subpackage Plugin_Name/includes
+ * @author     Your Name <email@example.com>
  */
-require plugin_dir_path( __FILE__ ) . 'classes/class-wp-chatbot.php';
+class WP_Chatbot extends WP_Chatbot_Base {
+	/* Singleton */
+
+	/**
+	 * Main plugin class instance
+	 *
+	 * Modelled after EDD, to insure that there is only one instance of the plugin at the time. Prevents globals. Follows singleton pattern.
+	 */
+	public static function instance() {
+		if ( ! isset( self::$instance ) ){
+
+			$class = self::class;
+
+			self::$instance = new $class;
+
+		}
+
+		return self::$instance;
+
+	}
+
+
+	public function __construct() {
+
+		parent::__construct();
+
+		require_once $this->path . '/classes/class-wp-chatbot-admin.php' ;
+		require_once $this->path . '/classes/class-wp-chatbot-public.php' ;
+		require_once $this->path . '/classes/class-wp-chatbot-request.php' ;
+		require_once $this->path . '/classes/class-wp-chatbot-conversation.php' ;
+
+		$this->define_admin_hooks();
+		$this->define_public_hooks();
+
+	}
+
+
+	/**
+	 * Register all of the hooks related to the admin area functionality
+	 * of the plugin.
+	 *
+	 * @since    0.1.0
+	 * @access   private
+	 */
+	private function define_admin_hooks() {
+
+		$plugin_admin = new WP_Chatbot_Admin();
+
+		add_action( 'admin_enqueue_scripts', array( $plugin_admin, 'enqueue_styles' ) );
+		add_action( 'admin_enqueue_scripts', array( $plugin_admin, 'enqueue_scripts' ) );
+
+		// Menus
+		add_action( 'admin_menu', array( $plugin_admin, 'admin_menu' ) );
+		add_action( 'admin_init', array( $plugin_admin, 'register_settings' ) );
+	}
+
+	/**
+	 * Register all of the hooks related to the public-facing functionality
+	 * of the plugin.
+	 *
+	 * @since    0.1.0
+	 * @access   private
+	 */
+	private function define_public_hooks() {
+
+		$plugin_public = new WP_Chatbot_Public( $this->slug, $this->version );
+
+		add_action( 'wp_enqueue_scripts', array( $plugin_public, 'enqueue_styles' ) );
+		add_action( 'wp_enqueue_scripts', array( $plugin_public, 'enqueue_scripts' ) );
+
+		// Ajax callbacks for conversation
+		add_action( 'wp_ajax_nopriv_wp_chatbot_converse', array( $plugin_public, 'wp_chatbot_converse' ) );
+		add_action( 'wp_ajax_wp_chatbot_converse', array( $plugin_public, 'wp_chatbot_converse' ) );
+
+		$options_general = get_option( 'wp-chatbot-options-general' );
+
+		if ( isset( $options_general[ 'chatbot-livechat']) and $options_general[ 'chatbot-livechat'] == '1' ) {
+			add_action( 'wp_footer', array( $plugin_public, 'chat_interface_livechat' ) );
+		} else {
+			add_shortcode( 'wp-chatbot', array( $plugin_public, 'chat_interface_shortcode' ));
+		}
+
+	}
+
+}
 
 /**
  * Begins execution of the plugin.
  *
- * Since everything within the plugin is registered via hooks,
- * then kicking off the plugin from this point in the file does
- * not affect the page life cycle.
+ * Can also be used to get the plugin singleton
  *
  * @since    0.1.0
  */
-function run_wp_chatbot() {
-	$plugin = new WP_Chatbot();
-	$plugin->run();
+function wp_chatbot() {
+
+	return WP_Chatbot::instance();
+
 }
-run_wp_chatbot();
+wp_chatbot();
 ?>
